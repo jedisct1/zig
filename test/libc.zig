379d128cba904a7599ebb191afa0873ef1a633cc
Jeff Anderson <kj4tmp@gmail.com>
2026-01-31 17:18:42 -0800

    libzigc: roundf

--- test/libc.zig
+++ test/libc.zig
@@ -297,7 +297,7 @@ pub fn addCases(cases: *tests.LibcContext) void {
     cases.addLibcTestCase("math/rintf.c", true, .{});
     // cases.addLibcTestCase("math/rintl.c", true, .{});
     cases.addLibcTestCase("math/round.c", true, .{});
-    // cases.addLibcTestCase("math/roundf.c", true, .{});
+    cases.addLibcTestCase("math/roundf.c", true, .{});
     cases.addLibcTestCase("math/roundl.c", true, .{});
     cases.addLibcTestCase("math/scalb.c", true, .{});
     cases.addLibcTestCase("math/scalbf.c", true, .{});
